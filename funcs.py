7f19eafa8e7c06af95fa61ce0e983244f323c98f
Margarita Rastriga <rasm@1Ci.com>
2023-04-22 21:32:34 +0300

    Add difference

--- funcs.py
+++ funcs.py
@@ -2,3 +2,9 @@ def sum_a_and_b(a, b):
     """get a, b
     returns summary of a and b"""
     return a + b
+
+
+def dif_a_and_b(a, b):
+    """get a, b
+    returns difference between a and b"""
+    return a - b
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-from funcs import sum_a_and_b
+from funcs import *
 
 print(sum_a_and_b(5, 49))
+print(dif_a_and_b(49, 5))
